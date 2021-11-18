da299e8e0dcda11fc2595836e7c8e7ec5662a9f5
EliottJaquier <61775618+EliottJaquierCPNV@users.noreply.github.com>
2021-11-18 14:37:08 +0100

    First commit

---
+++ Who/TestWho/TestPerson.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestWho
+{
+    public class Person
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Who/Who/Person.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Who
+{
+    public class Person
+    {
+
+    }
+}
